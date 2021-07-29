3a2089fbee7b812cfc4c49aae46e5e2c88d8efd7
exit <guilhermebelotto@gmail.com>
2021-07-28 21:21:51 -0300

    cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,197 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:agenda/data.dart';
+import 'package:agenda/persistence/file_persistence.dart';
+import 'package:agenda/persistence/key_value_persistence.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final PersistenceData data = new KeyValuePersistence();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final _textController = TextEditingController();
+  final _telefoneController = TextEditingController();
+  var _posRemoved = 0;
+  var _doToRemoved = new Map<String, dynamic>();
+
+  @override
+  void initState() {
+    super.initState();
+    data.load().then((value) {
+      if (value.isNotEmpty)
+        setState(() {
+          _toDoList = json.decode(value);
+        });
     });
   }
 
+  void _saveData() {
+    String jsonList = json.encode(_toDoList);
+    data.save(jsonList);
+  }
+
+  void _addTodo() {
+    setState(() {
+      var newTodo = Map<String, dynamic>();
+      newTodo["name"] = _textController.text;
+      newTodo["telefone"] = _telefoneController.text;
+      newTodo["ok"] = false;
+      _toDoList.insert(0, newTodo);
+      _textController.clear();
+      _telefoneController.clear();
+      _saveData();
+    });
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"]) return 1;
+        if (!a["ok"] && b["ok"]) return -1;
+        return 0;
+      });
+      _saveData();
+    });
+  }
+
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Lista de Contatos'),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        padding: EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: Column(
+                children: [
+                  TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                          labelText: "Novo Contato",
+                          labelStyle: TextStyle(color: Colors.blueAccent))),
+                  TextField(
+                      controller: _telefoneController,
+                      decoration: InputDecoration(
+                          labelText: "Telefone",
+                          labelStyle: TextStyle(color: Colors.blueAccent))),
+                  SizedBox(width: 4.0),
+                  ElevatedButton(
+                    onPressed: _addTodo,
+                    child: Text('ADD'),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: RefreshIndicator(
+                onRefresh: _refresh,
+                child: ListView.builder(
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        direction: DismissDirection.startToEnd,
+        // child: CheckboxListTile(
+        //   value: _toDoList[index]['ok'],
+        //   onChanged: (c) {
+        //     setState(() {
+        //       _toDoList[index]['ok'] = c;
+        //     });
+        //   },
+        //   title: Text(_toDoList[index]['telefone']),
+        //   secondary: Text(_toDoList[index]['name']),
+        // ),
+        child: Card(
+          child: Row(children: [            
+              Expanded(child: Text(_toDoList[index]['name'], style: TextStyle(color: Colors.blueAccent),)),
+              Expanded(child: Text(_toDoList[index]['telefone'], style: TextStyle(color: Colors.blueAccent),)),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            
+          ),
+          // child: ListTile(
+          //   title: Text(_toDoList[index]['name'], style: TextStyle(color: Colors.blueAccent),),
+          //   subtitle: Text(_toDoList[index]['telefone'], style: TextStyle(color: Colors.blueAccent),),
+          //   horizontalTitleGap: ,
+          // ),
+        ),
+        onDismissed: (_) {
+          setState(() {
+            _posRemoved = index;
+            _doToRemoved = Map.from(_toDoList[index]);
+            _toDoList.removeAt(index);
+            _saveData();
+
+            final snack = SnackBar(
+              content: Text('Contato \"${_doToRemoved["name"]}\" removido!'),
+              action: SnackBarAction(
+                label: 'Desfazer',
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_posRemoved, _doToRemoved);
+                    _saveData();
+                  });
+                },
+              ),
+              duration: Duration(seconds: 2),
+            );
+
+            ScaffoldMessenger.of(context).removeCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(snack);
+          });
+        });
   }
 }
